13d13d8a9c8338399d91551474b282a0f6ef7aa1
simone-precisionlife <simone@precisionlife.com>
2024-09-17 16:46:30 +0100

    Remove pylint workflow and add fixture test

--- test_sum.py
+++ test_sum.py
@@ -1,9 +1,20 @@
-"""
-Test function
-"""
+import pytest
 
-def test(x,y):
+# define fixture for test sum function
+@pytest.fixture
+def sum_fixture():
+    """
+    Fixture for sum function
+    """
+    x = 5
+    y = 10
+    sum = x + y  # calculate sum using fixture values
+    yield sum  # yield the sum to the test function
+
+def test_sum(sum_fixture):
     """
     Test file that prints sum
     """
-    print(x + y)
+    sum = sum_fixture  # get the sum from the fixture
+
+    assert sum == 15, f"Expected {15}, got {sum}"
